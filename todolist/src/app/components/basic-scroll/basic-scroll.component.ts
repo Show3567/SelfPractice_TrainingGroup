020723a7ded39abde35097c8971f7601c1c2b5f8
Yue Dong <ydong011@fiu.edu>
2023-02-20 23:56:14 +0800

    update

--- todolist/src/app/components/basic-scroll/basic-scroll.component.ts
+++ todolist/src/app/components/basic-scroll/basic-scroll.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-basic-scroll',
   templateUrl: './basic-scroll.component.html',
-  styleUrls: ['./basic-scroll.component.scss']
+  styleUrls: ['./basic-scroll.component.scss'],
 })
 export class BasicScrollComponent {
+  people: any;
 
+  constructor() {}
 }
