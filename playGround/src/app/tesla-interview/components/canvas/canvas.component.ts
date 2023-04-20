6611fa91aa16848b7bc27fddd9f85ad7f40609bf
Yue Dong <ydong011@fiu.edu>
2023-04-20 12:52:56 -0500

    update

--- playGround/src/app/tesla-interview/components/canvas/canvas.component.ts
+++ playGround/src/app/tesla-interview/components/canvas/canvas.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-canvas',
   templateUrl: './canvas.component.html',
-  styleUrls: ['./canvas.component.scss']
+  styleUrls: ['./canvas.component.scss'],
 })
-export class CanvasComponent {
+export class CanvasComponent implements OnInit {
+  @ViewChild('canvas', { static: true })
+  canvasRef!: ElementRef<HTMLCanvasElement> | any;
 
+  colors: string[] = [
+    'red',
+    'green',
+    'blue',
+    'yelllow',
+    'orange',
+    'purple',
+    'pink',
+    'brown',
+    'black',
+    'beige',
+  ];
+
+  ngOnInit(): void {
+    const canvas = this.canvasRef.nativeElement;
+    const ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = 'green';
+    ctx.fillRect(0, 0, 100, 100);
+  }
 }
