f074ac176b5078bcb54b51797e5c647acec5e826
Yue Dong <ydong011@fiu.edu>
2023-04-19 22:56:36 -0500

    update

--- playGround/src/app/app.module.ts
+++ playGround/src/app/app.module.ts
@@ -3,18 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TeslaInterviewCanvasComponent } from './tesla-interview-canvas/tesla-interview-canvas.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TeslaInterviewCanvasComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
