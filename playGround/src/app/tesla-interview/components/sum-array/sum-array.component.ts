8f2315e50d1b06ed363efc4e2766ac023dced07e
Yue Dong <ydong011@fiu.edu>
2023-04-19 23:15:06 -0500

    update

--- playGround/src/app/tesla-interview/components/sum-array/sum-array.component.ts
+++ playGround/src/app/tesla-interview/components/sum-array/sum-array.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sum-array',
   templateUrl: './sum-array.component.html',
-  styleUrls: ['./sum-array.component.scss']
+  styleUrls: ['./sum-array.component.scss'],
 })
-export class SumArrayComponent {
+export class SumArrayComponent implements OnInit {
+  arr = [1, 2, 3];
+  sumarr = 0;
 
+  ngOnInit(): void {
+    this.sum();
+  }
+
+  sumAndRemove() {
+    this.arr.pop();
+    this.sum();
+  }
+  sum() {
+    this.sumarr = this.arr.reduce((acc, cur) => acc + cur, 0);
+  }
 }
