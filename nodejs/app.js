6a900a064a4dcd96d93755d6cb8d7492b8f8e304
Yue Dong <ydong011@fiu.edu>
2023-05-14 16:27:52 -0500

    update

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,4 +1,21 @@
-const server = require('./resouse/server');
+// const server = require('./resouse/server');
+// const port = 3001;
+
+// server.listen(port);
+
+const express = require('express');
+const app = express();
+
 const port = 3001;
 
-server.listen(port);
+app.post('/api', (req, res) => {
+  console.log(111);
+  
+  res.send({
+    info: 'hello'
+  });
+});
+
+app.listen(port, (req, res, next) => {
+  console.log(`listening on port ${port}...`);
+});
