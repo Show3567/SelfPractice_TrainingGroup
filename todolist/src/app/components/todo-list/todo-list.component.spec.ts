7a3670709c305efcf36b9ecd2deb9ff947449b06
Yue Dong <ydong011@fiu.edu>
2023-05-24 14:12:30 -0500

    update

--- todolist/src/app/components/todo-list/todo-list.component.spec.ts
+++ todolist/src/app/components/todo-list/todo-list.component.spec.ts
@@ -5,6 +5,51 @@ import { TodoService } from 'src/app/services/todo.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { baseUrl } from 'src/app/app.module';
 import { FormsModule } from '@angular/forms';
+import { Todo } from 'src/app/interfaces/todo.interface';
+import { By } from '@angular/platform-browser';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
+import { of } from 'rxjs';
+
+const mocktodos: Todo[] = [
+  {
+    userId: 1,
+    id: 18,
+    title: 'dolorum est consequatur ea mollitia in culpa',
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 19,
+    title: 'molestiae ipsa aut voluptatibus pariatur dolor nihil',
+    completed: true,
+  },
+  {
+    userId: 1,
+    id: 20,
+    title: 'ullam nobis libero sapiente ad optio sint',
+    completed: true,
+  },
+];
+
+@Component({
+  selector: 'app-todo-item',
+  template: `<div>{{ todo.title }}</div>`,
+})
+class TodoItemComponent {
+  @Input() todo = {
+    userId: 1,
+    id: 20,
+    title: 'ullam nobis libero sapiente ad optio sint',
+    completed: true,
+  };
+  @Output() idemiter = new EventEmitter();
+}
 
 describe('TodoListComponent', () => {
   let component: TodolistComponent;
@@ -13,7 +58,7 @@ describe('TodoListComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HttpClientTestingModule, FormsModule],
-      declarations: [TodolistComponent],
+      declarations: [TodolistComponent, TodoItemComponent],
       providers: [
         TodoService,
         {
@@ -30,5 +75,47 @@ describe('TodoListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('View', () => {
+    it('should has a header', () => {
+      const headers = fixture.debugElement.queryAll(By.css('header'));
+
+      expect(headers.length).toBe(2);
+      expect(headers[0].classes['header']).toBe(true);
+      expect(headers[1].classes['todolist__header']).toBe(true);
+    });
+    it('should has a main', () => {
+      const mains = fixture.debugElement.queryAll(By.css('main'));
+
+      expect(mains.length).toBe(1);
+      expect(mains[0].classes['content']).toBe(true);
+    });
+    it('should has an inputbox', () => {
+      const inputs = fixture.debugElement.queryAll(By.css('input'));
+      expect(inputs.length).toBe(1);
+    });
+    it('should has an ul', () => {
+      const uls = fixture.debugElement.queryAll(By.css('ul'));
+      expect(uls.length).toBe(1);
+    });
+    it('should has some todoitem in the ul', () => {
+      component.todos$ = of(mocktodos);
+      fixture.detectChanges();
+
+      const itemeles = fixture.debugElement.queryAll(
+        By.css('ul app-todo-item')
+      );
+      expect(itemeles.length).toBe(mocktodos.length);
+    });
+    it('should has an button, click it will trigger sendNewRequest fn', () => {
+      const btns = fixture.debugElement.queryAll(By.css('button'));
+      expect(btns.length).toBe(1);
+
+      const sendNewRequest = spyOn(component, 'sendNewRequest');
+      btns[0].triggerEventHandler('click');
+
+      expect(sendNewRequest).toHaveBeenCalled();
+    });
   });
 });
