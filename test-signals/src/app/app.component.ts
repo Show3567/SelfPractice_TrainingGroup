97d820ee33d00fba3fdca9793fc935dc6e66184e
Yue Dong <ydong011@fiu.edu>
2023-06-21 20:09:42 -0500

    update

--- test-signals/src/app/app.component.ts
+++ test-signals/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'test-signals';
+  input = '';
+  title = signal<string>('test-signals');
+  items = new Array(20000).fill(0);
+
+  settitle() {
+    this.title.set(this.input);
+  }
 }
--- test-signals/src/app/app.module.ts
+++ test-signals/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './componensts/todo-list/todo-list.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, TodoListComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
