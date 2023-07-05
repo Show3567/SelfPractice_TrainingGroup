0fba93b301decef5262a6f735dccfc15773de37f
Yue Dong <ydong011@fiu.edu>
2023-07-05 15:31:29 -0500

    update

---
+++ standalone-playground/src/app/components/todo-item/todo-item.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-todo-item',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.scss']
+})
+export class TodoItemComponent {
+
+}
