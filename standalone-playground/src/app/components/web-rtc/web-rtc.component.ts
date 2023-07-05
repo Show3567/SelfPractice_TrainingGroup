5ced39044da97439bed5848e97ba55dc4bc688f5
Yue Dong <ydong011@fiu.edu>
2023-07-05 18:59:43 -0500

    update

---
+++ standalone-playground/src/app/components/web-rtc/web-rtc.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-web-rtc',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './web-rtc.component.html',
+  styleUrls: ['./web-rtc.component.scss']
+})
+export class WebRtcComponent {
+
+}
