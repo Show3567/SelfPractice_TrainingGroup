2580430a2efbf98bac5fe37d55e8b810d811b282
Yue Dong <ydong011@fiu.edu>
2023-07-21 21:16:42 -0500

    update

--- test-signals/src/app/components/testcarousel/testcarousel.component.ts
+++ test-signals/src/app/components/testcarousel/testcarousel.component.ts
@@ -7,35 +7,33 @@ import { Component } from '@angular/core';
 })
 export class TestcarouselComponent {
   private initnum = 6;
+  private i = this.initnum;
   private initarr = new Array(this.initnum).fill(0).map((ele, i) => ({
     id: i,
   }));
   list: any = [];
+  left = 0;
   position = 0 - this.initnum * 10;
-  useAnimation = true;
 
   ngOnInit(): void {
     this.list = [...this.initarr, ...this.initarr, ...this.initarr];
   }
 
   toRight() {
-    if (this.position / 10 === -1) {
-      this.useAnimation = false;
-      this.position = 0 - this.initnum * 10;
+    if (this.i === 0) {
+      this.position = this.position - this.initnum * 10;
+      this.i = this.initnum;
     }
-    this.position += 10;
+    this.i--;
+    this.left += 10;
   }
 
   toLeft() {
-    if (this.position / 10 === 0 - (2 * this.initnum - 1)) {
-      this.useAnimation = false;
-      this.position = 0 - this.initnum * 10;
+    if (this.i === this.initnum * 2) {
+      this.position = this.position + this.initnum * 10;
+      this.i = this.initnum;
     }
-    this.position -= 10;
-  }
-
-  transitionend(ele: HTMLElement) {
-    console.log(ele.style.transition);
-    this.useAnimation = true;
+    this.i++;
+    this.left -= 10;
   }
 }
